acd177cb42c9a0046402863588bd806dda543976
Valentin Fadeev <valentinfs@yandex.ru>
2021-09-01 21:46:35 +0300

    Formatting test

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,12 +1,13 @@
 package ru.netology.stats;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
     @Test
-     void shouldCalcSum() {
+    void shouldCalcSum() {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 180;
